9875d80288d3acd52d09aea350ce1ee7ab23a232
Callum Marshall <callum_marshall@hotmail.co.uk>
2019-05-08 12:13:02 +0100

    add database_helpers.rb and further user tests

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,11 +1,17 @@
 require 'user'
+require 'database_helpers'
 
 describe User do
 
   describe '.create' do
     it 'creates a new user' do
       user = User.create(email: 'test@example.com', password: 'password')
+
+      persisted_data = persisted_data(id: user.id)
+
       expect(user).to be_a User
+      expect(user.id).to eq persisted_data['id']
+      expect(user.email).to eq 'test@example.com'
     end
   end
 end
