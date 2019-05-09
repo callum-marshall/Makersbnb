079ec460e471905ea17cf59c8cb3776c992af900
Callum Marshall <callum_marshall@hotmail.co.uk>
2019-05-09 14:40:56 +0100

    fixed db migrations

--- db/migrations/01_create_databases.sql
+++
@@ -1,2 +0,0 @@
-CREATE DATABASE makersbnb_test;
-CREATE DATABASE makersbnb;
--- db/migrations/03_create_listings_table.sql
+++
@@ -1,1 +0,0 @@
-CREATE TABLE listings (id SERIAL PRIMARY KEY, owner_id int NOT NULL, description TEXT, name TEXT, price money);
